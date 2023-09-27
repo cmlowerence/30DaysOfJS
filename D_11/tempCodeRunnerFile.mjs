6412153a7f98ccf6713e4cb894c580ebf66a34d9
cmlowerence <cmlowerence2777@gmail.com>
2023-09-27 23:10:54 +0530

    D_12 Exercise till Level 2

--- D_11/tempCodeRunnerFile.mjs
+++ D_11/tempCodeRunnerFile.mjs
@@ -1,1 +1,8 @@
-const countries = ['Finland', 'Estonia', 'Sweden', 'Denmark', 'Norway']
+const sum = (...args) => {
+        let sum = 0;
+        for (let num of args) {
+            sum += num
+        }
+        return sum
+    }
+    console.log(sum(1, 2, 3, 4, 5, 6, 7, 8, 9))
