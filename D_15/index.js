d44c794d523cc265e5dcca48b49937257186a9de
cmlowerence <cmlowerence2777@gmail.com>
2023-10-01 23:13:07 +0530

    D_15 checkpoint

--- D_15/index.js
+++ D_15/index.js
@@ -102,3 +102,247 @@ class Person {
     console.log(person);
     console.log(person1);
 }
+
+// Class methods
+// The constructor inside a class is a builtin function which allow us to create a blueprint for the object. In a class we can create class methods. Methods are JavaScript functions inside the class. Let us create  some class methods
+{
+    class Person{
+        constructor(
+            firstName,lastName,age,country,city
+        ){
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.country = country;
+            this.city = city;
+        }
+        getFullName(){
+            const fullName = this.firstName + ' '+ this.lastName
+            return fullName
+        }
+    }
+
+    const person = new Person('Chudamani','Lawrence',20,'India','Mandi');
+    console.log(person.getFullName())
+}
+
+// Properties with initial values
+/* When we create a class for some properties we may have an initial value. For instance if ou are playing a game, ou start score will be zero. So, we may have a starting score or score which is zero. In other way, we may hae an initial skill and we will acquire some skill after some time. */
+
+{
+    class Person{
+        constructor(firstName,lastName,age,country,city){
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.country = country;
+            this.city = city;
+            this.score = 0; // Key with initial value
+            this.skills = [] // Key with initial value
+        }
+        getFullName(){
+            return `${this.firstName} ${this.lastName}`
+        }
+    }
+    const person1 = new Person('Chudamani','Lawrence',20,'India','Mandi');
+    const person2 = new Person('Rishi','Rathour',20,'India','Mandi');
+
+    console.log(person1.score);
+    console.log(person2.score);
+    console.log(person1.skills);
+    console.log(person2.skills);
+
+    // A method could me regular method or a getter or a setter. Let us see, getter and setter.
+
+    // 'getter' method
+    {
+        class Person{
+            constructor(firstName,lastName,age,country,city){
+                this.firstName = firstName;
+                this.lastName = lastName;
+                this.age = age;
+                this.country = country;
+                this.city = city;
+                this.score = 0;
+                this.skills = [];
+            }
+            getFullName(){
+                return `${this.firstName} ${this.lastName}`
+            }
+
+            // getter method
+            get getScore(){
+                return this.score;
+            }
+            get getSkills(){
+                return this.skills;
+            }
+        }
+        const person1 = new Person('Chudamani','Lawrence',20,'India','Mandi');
+        const person2 = new Person('Rishi','Rathour',20,'India','Mandi');
+
+        console.log(person1.getScore); // We do not require parenthesis to call a getter method
+        console.log(person2.getScore);
+
+        console.log(person1.getSkills);
+        console.log(person2.getSkills);
+
+        console.log(person1.getFullName()); // Require parenthesis to call this function
+        console.log(person2.getFullName());
+
+        // 'setter' method
+        // The setter method allows us to modify the value of certain properties. We can write a setter method using keyword set followed by a function.
+        {
+            class Person{
+                constructor(firstName,lastName,age,country,city){
+                    this.firstName = firstName;
+                    this.lastName = lastName;
+                    this.age = age;
+                    this.country =country;
+                    this.city = city;
+                    this.score = 0;
+                    this.skills = [];
+                }
+                getFullName(){
+                    return `${this.firstName} ${this.lastName}`;
+                }
+
+                // getter method
+                get getScore(){
+                    return this.score;
+                }
+                get getSkills(){
+                    return this.skills;
+                }
+
+                // setter method
+                set setScore(score){
+                    this.score += score
+                }
+                set setSkill(skill){
+                    this.skills.push(skill)
+                }
+            }
+
+            const person1 = new Person('Chudamani','Lawrence',20,'India','Mandi');
+            const person2 = new Person('Rishi','Rathour',20,'India','Mandi');
+
+            person1.setScore = 1
+            person1.setSkill = 'HTML';
+            person1.setSkill = 'CSS';
+            person1.setSkill = 'JavaScript';
+
+            person2.setScore = 1;
+            person2.setSkill = 'Planning';
+            person2.setSkill = 'Managing';
+            person2.setSkill = 'Organizing';
+
+            console.log(person1.score);
+            console.log(person2.score);
+            console.log(person1.skills);
+            console.log(person2.skills);
+
+        }
+
+        // Let us make a regular method called getPersonInfo in Person class
+        {
+            class Person{
+                constructor(firstName,lastName,age,country,city){
+                    this.firstName = firstName;
+                    this.lastName = lastName;
+                    this.age = age;
+                    this.country = country;
+                    this.city = city;
+                    this.score = 0;
+                    this.skills = [];
+                }
+                getFullName(){
+                    return `${this.firstName} ${this.lastName}`
+                }
+
+                get getScore(){
+                    return this.score;
+                }
+                get getSkills(){
+                    return this.skills;
+                }
+
+                set setScore(score){
+                    this.score += score;
+                }
+                set setSkill(skill){
+                    this.skills.push(skill)
+                }
+
+                getPersonInfo(){
+                    let fullName = `${this.firstName} ${this.lastName}`;
+                    let skills = this.skills.length>0&&this.skills.slice(0,this.skills.length-1).join(', ')+ ` and ${this.skills[this.skills.length-1]}`;
+                    let formattedSkills = skills?`He knows ${skills}`: '';
+                    
+                    let info = `${fullName} is ${this.age}. He lives ${this.city}, ${this.country}, ${formattedSkills}`;
+                    return info
+                }
+            }
+
+            const person1 = new Person('Chudamani','Lawrence',20,'India','Mandi');
+            const person2 = new Person('Rishi','Rathour',20,'India','Mandi');
+            const person3 = new Person('Virender','Kumar',22,'India','Mandi');
+
+            person1.setScore = 1;
+            person1.setSkill = 'HTML';
+            person1.setSkill = 'CSS';
+            person1.setSkill = 'JavaScript';
+            
+            person2.setScore = 1;
+            person2.setSkill = 'Planning';
+            person2.setSkill = 'Managing';
+            person2.setSkill = 'Organizing';
+
+            console.log(person1.getScore);
+            console.log(person2.getScore)
+
+            console.log(person1.getSkills)
+            console.log(person2.getSkills)
+            console.log(person3.getSkills)
+
+            console.log(person1.getPersonInfo())
+            console.log(person2.getPersonInfo())
+            console.log(person3.getPersonInfo())
+        }
+
+
+    }
+
+    // Static Methods
+        // The static keyword defines a static method for a class. Static methods are not called on instances of the class. Instead, they are called on the class itself. These are often utility functions, such as functions to create or clone objects. 
+    {
+        class Person{
+            constructor(firstName,lastName,age,country,city){
+                this.firstName = firstName;
+                this.lastName = lastName;
+                this.age = age;
+                this.country = country;
+                this.city = city;
+                this.score = 0;
+                this.skills = [];
+            }
+            getFullName(){
+                return `${this.firstName} ${this.lastName}`
+            }
+
+            get getScore(){
+                return this.score;
+            }
+            get getSkills{
+                return this.skills
+            }
+
+            set setScore(score){
+                this.score += score;
+            }
+            set setSkill(skill){
+                this.skills.push(skill)
+            }
+        }
+    }
+}
