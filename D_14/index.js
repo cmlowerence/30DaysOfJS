61c94367bdded0d9c785fc6bb101f82dbc234842
cmlowerence <cmlowerence2777@gmail.com>
2023-10-01 20:38:20 +0530

    D_14 Exercise

--- D_14/index.js
+++ D_14/index.js
@@ -1,4 +1,5 @@
 // @ts-check
+// const prompt = require('prompt-sync')();
 /* Error Handling */
 // JavaScript is loosely-typed language. Some times you will get a runtime error when you try to access an undefined variable or call undefined function etc.
 // JavaScript similar to python or Java provides an error-handling mechanism to catch runtime errors using try-catch-finally block
@@ -55,33 +56,98 @@ finally: finally block will always be executed regardless of the occurrence of a
     // throw true; // Generates an exception with the value true
     // throw new Error('Required'); // Generates an error object with the message of Required
 
-    const throwErrorExampleFun = ()=>{
-        let message;
-        let x = prompt('Enter a number: ');
-        try{
-            if (x=='') throw 'Empty';
-            if(isNaN(x)) throw 'not a number'
-            x = Number(x);
-            if (x<5) throw 'too low';
-            if (x>10) throw 'too high'
-        }catch (err) {
-            console.log(err)
-        }
-    }
-    throwErrorExampleFun()
+    // const throwErrorExampleFun = ()=>{
+    //     let message;
+    //     let x = prompt('Enter a number: ');
+    //     try{
+    //         if (x=='') throw 'Empty';
+    //         if(isNaN(x)) throw 'not a number'
+    //         x = Number(x);
+    //         if (x<5) throw 'too low';
+    //         if (x>10) throw 'too high'
+    //     }catch (err) {
+    //         console.log(err)
+    //     }
+    // }
+    // throwErrorExampleFun()
 }
 
 // Error types
+
+
 // => ReferenceError: An illegal reference has occurred. A ReferenceError is thrown if we use a variable that has not been declared
 {
-    let firstName = 'Chudamani';
-    let fullName = firstName + ' ' + lastName;
-    console.log(fullName)
+    try{
+        let firstName = 'Chudamani';
+        let fullName = firstName + ' ' + lastName;
+    }catch(err){
+        console.log(err.name,err.message);
+    }
 }
 
 // SyntaxError: A syntax error has occurred
+// {
+//     let square = 2*2;
+//     console.log(square);
+//     console.log('Hello, world');
+// }
+
+// TypeError: A type error has occurred
 {
-    let square = 2*2;
-    console.log(square);
-    console.log('Hello, world');
+    // let num = 30;
+    // console.log(num.toLowerCase());
+}
+
+
+/* ========================= Exercise ============================= */
+console.log('%c ================= Exercise =================', 'font-weight: bold; font-size: 20px;color: red; text-shadow: 1px 1px 0 rgb(217,31,38); margin-bottom: 5px; padding: 5px;');
+{
+    const Q_1 = 'Write code that handles the exception when an array is empty and throws a custom exception with the message "Array is empty."'
+    console.log('%cindex.js line:106 Q_1', 'color: white; font-size: 16px; background-color: #007acc;', `${Q_1}`);
+    {
+        const throwErrMtArr= (arr)=>{
+            if(arr.length ===0){
+                throw 'Array is empty'
+            }
+        }
+        let myArr = [1,2,3,4];
+        let myArr1 = [];
+        try{
+            throwErrMtArr(myArr);
+            throwErrMtArr(myArr1)
+        }catch (err){
+            console.error(err)
+        }
+    }
+    
+    const Q_2 = ' Write code that handles the exception when an element in the array is not a number and throws a custom exception with the message "Invalid input: element at index [index] is not a number."'
+    console.log('%cindex.js line:120 Q_2', 'color: white; background-color: #007acc;', Q_2);
+    {
+        const NaNError = (arr)=>{
+            arr.forEach((e,i)=>{
+                if (typeof(e) != 'number'){
+                    const err = new Error();
+                    err.name = 'Invalid input';
+                    err.message = `element at index ${i} is not a number`;
+                    throw err;
+                }
+            })
+        }
+        const array = [67,'shyam','shubham',34];
+        try{
+            NaNError(array)
+        }catch (error){
+            console.log(error)
+        }finally{
+            console.log('Code block completed');
+        }
+    }
+
+    const Q_3 = 'Write code that handles the exception when you try to access an undefined variable and throws a custom exception with the message "Variable is undefined."'
+    console.log('%cindex.js line:147 Q_3', 'color: white; background-color: #007acc;', Q_3);
+    {
+        const varUndefined = (variable){
+            
+        }
+    }
 }
