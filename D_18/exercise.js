210f5c419871c0889283dcea06fad28ac521222a
cmlowerence <cmlowerence2777@gmail.com>
2023-10-05 19:18:31 +0530

    D_18 Exercise Chapter completed

--- D_18/exercise.js
+++ D_18/exercise.js
@@ -5,30 +5,30 @@
 
 
     // Exercise Level 1
-    // {
-    //     const Q_1 = 'Read the countries API using fetch and print the name of country, capital, languages, population and area'
-    //     console.log('%cexercise.js line:10 Q_1', 'color: white; background-color: #007acc;', Q_1);
-    //     {
-    //         fetch(countriesAPI)
-    //         .then(res=>res.json())
-    //         .then(data=>{
-    //             for (const c of data){
-    //                 console.log('Name:- ',c.name)
-    //                 console.log('Capital:- ',c.capital)
-    //                 let languages = [];
-    //                 for (const langObj of c.languages){
-    //                     languages.push(langObj.name);
-    //                 }
-    //                 console.log('Languages:- ',languages.join(', '))
+    {
+        const Q_1 = 'Read the countries API using fetch and print the name of country, capital, languages, population and area'
+        console.log('%cexercise.js line:10 Q_1', 'color: white; background-color: #007acc;', Q_1);
+        {
+            fetch(countriesAPI)
+            .then(res=>res.json())
+            .then(data=>{
+                for (const c of data){
+                    console.log('Name:- ',c.name)
+                    console.log('Capital:- ',c.capital)
+                    let languages = [];
+                    for (const langObj of c.languages){
+                        languages.push(langObj.name);
+                    }
+                    console.log('Languages:- ',languages.join(', '))
 
-    //                 console.log('Population:- ',c.population)
-    //                 console.log('Area:- ',c.area)
-    //                 console.log('\n\n========Separator=========') 
-    //             }
-    //         })
-    //         .catch(err=>console.error(err))
-    //     }
-    // }
+                    console.log('Population:- ',c.population)
+                    console.log('Area:- ',c.area)
+                    console.log('\n\n========Separator=========') 
+                }
+            })
+            .catch(err=>console.error(err))
+        }
+    }
 
     // Exercise Level 2
     {
