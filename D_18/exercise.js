263154ac3e41502e5d985ad5ae24845142f34c13
cmlowerence <cmlowerence2777@gmail.com>
2023-10-05 17:15:09 +0530

    D_18 Exercise Level 1 completed

--- D_18/exercise.js
+++ D_18/exercise.js
@@ -10,10 +10,23 @@
         console.log('%cindex.js line:232 Q_1', 'color: white; background-color: #007acc;', Q_1);
         {
             fetch(countriesAPI)
-            .then(res=>res.json)
+            .then(res=>res.json())
             .then(data=>{
-                console.log(data)
+                for (const c of data){
+                    console.log('Name:- ',c.name)
+                    console.log('Capital:- ',c.capital)
+                    let languages = [];
+                    for (const langObj of c.languages){
+                        languages.push(langObj.name);
+                    }
+                    console.log('Languages:- ',languages.join(', '))
+
+                    console.log('Population:- ',c.population)
+                    console.log('Area:- ',c.area)
+                    console.log('\n\n========Separator=========') 
+                }
             })
+            .catch(err=>console.error(err))
         }
     }
 }
