a22cccaa46726b637599cec65f64a2ed6f02003d
cmlowerence <cmlowerence2777@gmail.com>
2023-10-07 21:21:54 +0530

    D_20 Completed

--- D_20/index.js
+++ D_20/index.js
@@ -235,37 +235,94 @@ let element = {
 }
 
 // Conditional
-    // We say if, if else, if else if else, switch and ternary operators previous challenges
+// We say if, if else, if else if else, switch and ternary operators previous challenges
 
 {
     // syntax
     let condition = true;
-    if (condition){
+    if (condition) {
         // This part of code run for truthy condition
-    }else{
+    } else {
         // This part of code run for false condition
     }
 
     // if else
     let num = 5;
-    if (num>0){
+    if (num > 0) {
         console.log(`${num} is a positive number`);
-    }else{
+    } else {
         console.log(`${num} is a negative number`)
     }
 
     // if else if else if else
     let a = 0;
-    if (a>0){
+    if (a > 0) {
         console.log(`${a} is a positive number`)
-    }else if (a<0){
+    } else if (a < 0) {
         console.log(`${a} is a negative number`);
-    }else if (a===0){
+    } else if (a === 0) {
         console.log(`${a} is zero`)
-    }else{
+    } else {
         console.log(`${a} is not a number`)
     }
 }
 
 // Switch Examples
 
+{
+    let dayUserInput = "Monday";
+    let day = dayUserInput.toLowerCase();
+    switch (day) {
+        case 'monday':
+            console.log('Today is Monday');
+            break
+        case 'tuesday':
+            console.log('Today is Tuesday');
+            break
+        case 'wednesday':
+            console.log('Today is Wednesday');
+            break
+        case 'thursday':
+            console.log('Today is Thursday');
+            break
+        case 'Friday':
+            console.log('Today is Friday');
+            break
+        case 'saturday':
+            console.log('Today is Saturday');
+            break
+        case 'sunday':
+            console.log('Today is Sunday');
+            break
+        default:
+            console.log('It is not a weak day.')
+    }
+}
+
+// ternary
+{
+    let isRaining = true;
+    isRaining
+        ? console.log('You need a rain coat')
+        : console.log('No need for a rain coat.')
+}
+
+// Classes
+// We declare class with CamelCase which starts with capital letter
+
+// syntax
+class ClassName {
+    // code goes here
+}
+
+{
+    // defining class
+    class Person {
+        constructor(firstName = 'John', lastName = 'Doe') {
+            console.log(this) // checking output form here
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+    }
+    console.log(new Person())
+}
