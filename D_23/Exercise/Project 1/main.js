d7ac24770550143427ba43724ba75260f6228fc8
cmlowerence <cmlowerence2777@gmail.com>
2023-10-13 20:02:24 +0530

    D_23 Exercise Project 1 Setup

---
+++ D_23/Exercise/Project 1/main.js
@@ -0,0 +1,12 @@
+console.log('hello')
+const wrapper = document.querySelector('.wrapper')
+// ? Adding input field and buttons
+{
+    const msgDiv = document.createElement('span');
+    msgDiv.classList.add = 'msgDiv';
+    const input = document.createElement('input');
+    input.classList.add = 'input';
+    const button = document.createElement('button');
+    button.classList.add = 'button';
+    button.innerText = 'Generate Number';
+}
