4ba1addce2c08e3df9e878f9f024fa491de2e387
cmlowerence <cmlowerence2777@gmail.com>
2023-10-23 21:24:12 +0530

    D_28 Project Complete

--- D_28/index.js
+++ D_28/index.js
@@ -21,26 +21,82 @@ const addCandidate = (fName, lName, country, score) => {
         hour: "2-digit",
         minute: "2-digit",
     });
-    return `
-    <div class = "candidate">
-        <span class = "details">
-            <span class = "name-wrapper">
-                <span class = "name">${properCase(fName)} ${properCase(
-        lName
-    )}</span>
-                <span class = "date">${date}</span>
-            </span>
-            <span class = "country">${properCase(country)}</span>
-            <span class = "score">${score}</span>
-        </span>
-        <span class = "options">
-            <button onclick = "rmCandidate" class="btn_round">🗑</button>
-            <button onclick = "addScore" class="btn_round">+5</button>
-            <button onclick = "rmScore" class="btn_round">-5</button>
-        </span>
-    </div>`;
+    const candidateDiv = document.createElement("div");
+    candidateDiv.classList.add("candidate");
+    {
+        // Details Section
+        const detailsDiv = document.createElement("span");
+        detailsDiv.classList.add("details");
+        // Name Wrapper
+        const nameWrapper = document.createElement("span");
+        nameWrapper.classList.add("name-wrapper");
+        // Full Name Div
+        const nameDiv = document.createElement("span");
+        nameDiv.classList.add("name");
+        nameDiv.innerText = properCase(fName) + " " + properCase(lName);
+
+        // Date Div
+        const dateDiv = document.createElement("span");
+        dateDiv.classList.add("date");
+        dateDiv.innerText = date;
+
+        // Appending Child Elements to Parent Name Wrapper
+        nameWrapper.appendChild(nameDiv);
+        nameWrapper.appendChild(dateDiv);
+
+        // Country Element
+        const countryDiv = document.createElement("span");
+        countryDiv.classList.add("country");
+        countryDiv.innerText = properCase(country);
+
+        // Score Element
+        const scoreDiv = document.createElement("span");
+        scoreDiv.classList.add("score");
+        scoreDiv.innerText = properCase(score);
+
+        // Appending Child Elements to Parent Details Section
+        detailsDiv.appendChild(nameWrapper);
+        detailsDiv.appendChild(countryDiv);
+        detailsDiv.appendChild(scoreDiv);
+
+        // Option Section
+        const optionsDiv = document.createElement("span");
+        optionsDiv.classList.add("options");
+        
+        // Removing Candidate Button
+        const rmCandidateBtn = document.createElement("button");
+        rmCandidateBtn.classList.add("btn_round");
+        rmCandidateBtn.innerText = "🗑";
+
+        // Adding Score +5
+        const addScoreBtn = document.createElement("button");
+        addScoreBtn.classList.add("btn_round");
+        addScoreBtn.innerText = "+5";
+
+        // Subtracting Score -5
+        const subScoreBtn = document.createElement("button");
+        subScoreBtn.classList.add("btn_round");
+        subScoreBtn.innerText = "-5";
+        // Some Event Listeners on buttons
+        rmCandidateBtn.addEventListener("click", () => candidateDiv.remove());
+        addScoreBtn.addEventListener("click", () => {
+            scoreDiv.innerText = 5 + +scoreDiv.innerText;
+        });
+        subScoreBtn.addEventListener("click", () => {
+            scoreDiv.innerText = -5 + +scoreDiv.innerText;
+        });
+
+        // Appending Child Elements to Parent Options Section
+        optionsDiv.appendChild(rmCandidateBtn);
+        optionsDiv.appendChild(addScoreBtn);
+        optionsDiv.appendChild(subScoreBtn);
+        // Appending Child Elements to Parent Candidate
+        candidateDiv.appendChild(detailsDiv);
+        candidateDiv.appendChild(optionsDiv);
+    }
+    return candidateDiv;
 };
-console.log("hello");
+
 smtBtn.addEventListener("click", () => {
     let isValidScore = !isNaN(parseInt(score.value));
     let intScore;
@@ -58,11 +114,8 @@ smtBtn.addEventListener("click", () => {
         isValidScore
     ) {
         err.style.display = "none";
-        container.innerHTML += addCandidate(
-            fName.value,
-            lName.value,
-            country.value,
-            score.value
+        container.appendChild(
+            addCandidate(fName.value, lName.value, country.value, score.value)
         );
         fName.value = "";
         lName.value = "";
